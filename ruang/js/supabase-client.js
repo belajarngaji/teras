ab869d508bbab93b4a33563cd15f33b02a83914f
belajarngaji <ffarid.5213@gmail.com>
2025-08-31 00:38:56 +0700

    Update supabase-client.js

--- ruang/js/supabase-client.js
+++ ruang/js/supabase-client.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = https://jpxtbdawajjyrvqrgijd.supabase.co;
-const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI4OTgsImV4cCI6MjA3MTg4ODg5OH0.vEqCzHYBByFZEXeLIBqx6b40x6-tjSYa3Il_b2mI9NE;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI4OTgsImV4cCI6MjA3MTg4ODg5OH0.vEqCzHYBByFZEXeLIBqx6b40x6-tjSYa3Il_b2mI9NE;
 
 const { createClient } = supabase;
-window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
