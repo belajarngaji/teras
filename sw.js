3ed7e3e268ac0b1c03770dbe1932ae699ca579a7
belajarngaji <ffarid.5213@gmail.com>
2025-09-03 11:24:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,34 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'belajar-ngaji-cache-v1';
+const urlsToCache = [
+  '/',
+  '/teras/',
+  '/teras/index.html',
+  '/teras/manifest.json',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
