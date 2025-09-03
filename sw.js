3e10745dad22c0c49e0452297a235650a7e74ec7
belajarngaji <ffarid.5213@gmail.com>
2025-09-03 11:08:54 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  // bisa tambah caching di sini jika mau
+});
