0a027d25a7a8d66d3ae3b10640128f99a255e1ed
belajarngaji <ffarid.5213@gmail.com>
2025-09-13 16:15:43 +0700

    Update supabaseclient.js

--- assets/js/supabaseclient.js
+++ assets/js/supabaseclient.js
@@ -1,3 +1,5 @@
+  // --- assets/js/supabaseclient.js
+
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://jpxtbdawajjyrvqrgijd.supabase.co";
